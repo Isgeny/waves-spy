67ee45344df7484ad7b5a88f4ad4d73791f5d6cf
Isgeny <jack091196@gmail.com>
2022-07-23 00:55:21 +0300

    Hello visjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from "react";
 import './App.css';
+import { Network } from "vis-network";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App: React.FC = () => {
+  const nodes = [
+		{ id: 1, label: "Node 1" },
+		{ id: 2, label: "Node 2" },
+		{ id: 3, label: "Node 3" },
+		{ id: 4, label: "Node 4" },
+		{ id: 5, label: "Node 5" },
+	];
+
+	const edges = [
+		{ from: 1, to: 3 },
+		{ from: 1, to: 2 },
+		{ from: 2, to: 4 },
+		{ from: 2, to: 5 },
+		{ from: 3, to: 3 },
+	];
+
+  const data = { nodes, edges };
+
+  const options = { };
+
+	const visJsRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {   
+    const network = visJsRef.current &&	new Network(visJsRef.current, data, options);
+	});
+
+	return (
+    <div className="network" ref={visJsRef}></div>
   );
-}
+};
 
 export default App;
